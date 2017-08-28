6ab958542864daa2a2123f838194ed9b2974eeb7
Labi132 <alexander_eder@gmx.net>
2017-08-28 10:18:25 +0200

    changedData file added, extracts wanted columns. toDo: merge tuples to one and add csv writer fixed missing rating in changedData, now all wanted ratings extracted

--- InfSys2017_Projekt_Thema7/dataChange.py
+++ InfSys2017_Projekt_Thema7/dataChange.py
@@ -8,10 +8,13 @@ def changeData():
     changeOne = [tuple(line)[len(line) - 8:] for line in rawData]  # drops columns left of overall Rating
     changeTwo = [tuple(line)[:len(line) - 6] for line in changeOne]  # speichert spalte 0 und 1
     print changeTwo[0]
+    print changeTwo[2]
     changeThree = [tuple(line)[len(line) - 2:len(line) - 1:] for line in changeOne]
     print changeThree[0]
-    changeFour = [tuple(line)[len(line) - 4:len(line) - 3:] for line in changeOne]
+    print changeThree[2]
+    changeFour = [tuple(line)[len(line) - 5:len(line) - 3:] for line in changeOne]
     print changeFour[0]
+    print changeFour[2]
     # print dir(tuple)
    # print dir(list)
 
