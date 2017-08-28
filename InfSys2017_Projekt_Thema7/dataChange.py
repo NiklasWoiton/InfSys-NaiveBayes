c9d496fcbe0c7b60847b24e08616a759d53b5692
Labi132 <alexander_eder@gmx.net>
2017-08-28 12:20:48 +0200

    added for-loop to get completed needed datasets

--- InfSys2017_Projekt_Thema7/dataChange.py
+++ InfSys2017_Projekt_Thema7/dataChange.py
@@ -15,9 +15,12 @@ def changeData():
     changeFour = [tuple(line)[len(line) - 2:len(line) - 1:] for line in changeOne]  # saves column 6
     print changeFour[0]
     print changeFour[2]
-    # writer = csv.writer(open("wordData.csv", "w"))
-    # for x in range(0, changeOne.count(), 1):
-        # writer.
+    writer = csv.writer(open("wordData.csv", "w"))
+    for x in range(0, len(changeOne), 1):
+        toDoRow = (changeTwo[x]+changeThree[x]+changeFour[x])
+        print toDoRow
+        # writer.writerow(toDoRow)
+        x += x
 
 
 
