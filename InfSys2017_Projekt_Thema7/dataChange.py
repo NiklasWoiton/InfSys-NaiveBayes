e4e9f65e9d4506a5732eb90570180158a43110a6
Labi132 <alexander_eder@gmx.net>
2017-08-28 10:14:39 +0200

    changedData file added, extracts wanted columns. toDo: merge tuples to one and add csv writer

---
+++ InfSys2017_Projekt_Thema7/dataChange.py
@@ -0,0 +1,18 @@
+import csv
+from itertools import chain, product
+
+
+# 0 1 3 4 6 Keep in changeOne aka keep Overall, value, location. cleanliness, service; drop rooms, fronst desk and buisness service
+def changeData():
+    rawData = csv.reader(open("rawData.csv", "rb"))
+    changeOne = [tuple(line)[len(line) - 8:] for line in rawData]  # drops columns left of overall Rating
+    changeTwo = [tuple(line)[:len(line) - 6] for line in changeOne]  # speichert spalte 0 und 1
+    print changeTwo[0]
+    changeThree = [tuple(line)[len(line) - 2:len(line) - 1:] for line in changeOne]
+    print changeThree[0]
+    changeFour = [tuple(line)[len(line) - 4:len(line) - 3:] for line in changeOne]
+    print changeFour[0]
+    # print dir(tuple)
+   # print dir(list)
+
+changeData()
